7e4799690eabc27a95298337bf0e6ede5090e54c
Carolyn Sy <csy@Carolyns-MacBook-Pro.local>
2016-02-15 21:32:38 -0800

    submitting hw2

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -12,6 +12,19 @@ class KeyboardViewController: UIInputViewController {
 
     @IBOutlet var nextKeyboardButton: UIButton!
     
+    @IBOutlet weak var returnButton: UIButton!
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var anotherButton: UIButton!
+    
+    @IBOutlet weak var yetAnotherButton: UIButton!
+    
+    @IBOutlet weak var andAnotherButton: UIButton!
+    
+    @IBOutlet weak var dumbButton: UIButton!
+    
+    
     var keyboardView: UIView!
 
     override func updateViewConstraints() {
@@ -23,6 +36,7 @@ class KeyboardViewController: UIInputViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadInterface()
+        loadButtons()
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,6 +60,39 @@ class KeyboardViewController: UIInputViewController {
         view.backgroundColor = keyboardView.backgroundColor
         nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
         
+    }
+    
+    func loadButtons() {
+        returnButton.addTarget(self, action: "pressReturn", forControlEvents: .TouchUpInside)
+        deleteButton.addTarget(self, action: "deleteStuff", forControlEvents: .TouchUpInside)
+        anotherButton.addTarget(self, action: "typeStuff", forControlEvents: .TouchUpInside)
+        yetAnotherButton.addTarget(self, action: "typeMoreStuff", forControlEvents: .TouchUpInside)
+        andAnotherButton.addTarget(self, action: "typeEvenMoreStuff", forControlEvents: .TouchUpInside)
+        dumbButton.addTarget(self, action: "lastStuff", forControlEvents: .TouchUpInside)
+    }
+    
+    func pressReturn() {
+        (textDocumentProxy as UIKeyInput).insertText("\n")
+    }
+    
+    func deleteStuff() {
+        (textDocumentProxy as UIKeyInput).deleteBackward()
+    }
+    
+    func typeStuff() {
+        (textDocumentProxy as UIKeyInput).insertText("╭∩╮（︶︿︶）╭∩╮")
+    }
+    
+    func typeMoreStuff() {
+        (textDocumentProxy as UIKeyInput).insertText("(づ｡◕‿‿◕｡)づ")
+    }
+    
+    func typeEvenMoreStuff() {
+        (textDocumentProxy as UIKeyInput).insertText("Sorry this keyboard is ugly AF. Pls don't fail me")
+    }
+    
+    func lastStuff() {
+        (textDocumentProxy as UIKeyInput).insertText("Yᵒᵘ Oᶰˡʸ Lᶤᵛᵉ Oᶰᶜᵉ")
     }
 
 
